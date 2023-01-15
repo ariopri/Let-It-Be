9131b041c1395aacfc95acea04605bc73f932888
ariopri <arioprima1@gmail.com>
2023-01-15 14:59:43 +0700

    refactor : change subject struct code

--- backend/models/pelajaran.go
+++ backend/models/pelajaran.go
@@ -3,10 +3,11 @@ package models
 import "context"
 
 type Subject struct {
-	ID          int64  `json:"id"`
-	Name        string `json:"name"`
-	Image       string `json:"image"`
-	Description string `json:"description"`
+	ID          int64    `json:"id"`
+	Name        string   `json:"name"`
+	Image       string   `json:"image"`
+	Description string   `json:"description"`
+	Classes     []*Class `json:"classes"`
 }
 
 type Class struct {
@@ -19,6 +20,8 @@ type Class struct {
 type SubjectRepository interface {
 	GetAll(ctx context.Context) ([]*Subject, error)
 	GetByID(ctx context.Context, id int64) (*Subject, error)
+	GetByName(ctx context.Context, name string) (*Subject, error)
+	GetSubjectWithClasses(ctx context.Context) ([]*Subject, error)
 	Create(ctx context.Context, subject *Subject) (*Subject, error)
 	Update(ctx context.Context, id int64, subject *Subject) (*Subject, error)
 	Delete(ctx context.Context, id int64) error
@@ -27,7 +30,9 @@ type SubjectRepository interface {
 type ClassRepository interface {
 	GetAll(ctx context.Context) ([]*Class, error)
 	GetByID(ctx context.Context, id int64) (*Class, error)
+	GetByName(ctx context.Context, name string) (*Class, error)
 	GetBySubjectID(ctx context.Context, subjectID int64) ([]*Class, error)
+	GetBySubjectName(ctx context.Context, subjectName string) ([]*Class, error)
 	Create(ctx context.Context, class *Class) (*Class, error)
 	Update(ctx context.Context, id int64, class *Class) (*Class, error)
 	Delete(ctx context.Context, id int64) error
