5bbf473c883fbbdf2ee155df4e133db978fe54e0
ariopri <arioprima1@gmail.com>
2023-01-19 00:02:26 +0700

    feat : add some route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import { Outlet, Route, Routes } from 'react-router-dom';
 import RoutesOutlet from './utils/RoutesOutlet';
 import LandingPage from './pages/01-LandingPage/LandingPage';
 import Informasi from './pages/02-InformasiPage/Informasi';
-import HubungiKami from './pages/03-Hubungi/Hubungi';
 import About from './pages/04-Tentang/About';
+import HubungiKami from './pages/03-Hubungi/Hubungi';
 import Modul from './pages/05-Modul/Modul';
 import Register from './pages/07-Daftar/Register';
 import CaraMendafatarPengajar from './pages/08-DaftarPengajar/CaraDaftarPengajar';
 import Protected from './protected/protectedRoute';
 import Login from './pages/06-LoginPage/Login';
 import ModulDetail from './pages/09-ModulDetail/ModulDetail';
+import SocialProfileSimple from './pages/10-ProfilePage/Profile';
+import NotFound from './pages/11-NotFound/NotFound';
+import Event from './pages/12-Event/Event';
 
 function App() {
   return (
@@ -19,13 +22,16 @@ function App() {
         <Route path="informasi" element={<Informasi />} />
         <Route path="hubungi" element={<HubungiKami />} />
         <Route path="tentang" element={<About />} />
-
-        <Route path="modul" element={<Outlet />}>
-          <Route index element={<Modul />} />
-          <Route path=":name" element={<Outlet />}>
-            <Route path=":id" element={<ModulDetail />} />
+        <Route element={<Protected />}>
+          <Route path="modul" element={<Outlet />}>
+            <Route index element={<Modul />} />
+            <Route path=":name" element={<Outlet />}>
+              <Route path=":id" element={<ModulDetail />} />
+            </Route>
           </Route>
         </Route>
+        <Route path="event" element={<Event />} />
+        <Route path="profile" element={<SocialProfileSimple />} />
         <Route path="masuk" element={<Login />} />
         <Route path="mendaftar" element={<Register />} />
         <Route
@@ -33,6 +39,7 @@ function App() {
           element={<CaraMendafatarPengajar />}
         />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
